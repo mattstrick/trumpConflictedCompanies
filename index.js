b84f2abec852ff2dc785e81c4b65f92902761834
Matt Strick <matt.strick@gmail.com>
2019-11-07 21:30:08 -0600

    FEAT: added existing list of companies

---
+++ index.js
@@ -0,0 +1,261 @@
+[
+    {
+      "id": "cubs",
+      "brand": "Cubs",
+      "description": "Betsy Devos owns a stake in many parts of the Cubs economy including the team and its various operations, Wrigley Field,  Comcast SportsNet Chicago, and land and property in Wrigleyville",
+      "sourceDescription": "2018 Financial Disclosures",
+      "source": "https://www.opensecrets.org/trump/financial-disclosures"
+    },
+    {
+      "id": "amway",
+      "brand": "Amway",
+      "description": "Betsy Devos's family owns Amway.",
+      "sourceDescription": "Betsy Devos's 2018 Financial Disclosures",
+      "source": "https://www.opensecrets.org/trump/financial-disclosures"
+    },
+    {
+      "id": "trump",
+      "brand": "Trump",
+      "description": "Donald Trump's businesses which he refused to divest from, unlike every other President",
+      "sourceDescription": "Reuter's article",
+      "source": "https://www.reuters.com/article/us-usa-trump-finance/trump-says-wont-divest-from-his-business-while-president-idUSKBN14V21I"
+    },
+    {
+      "id": "equinox",
+      "brand": "Equinox",
+      "description": "Billionaire chairman of Related Companies Stephen M. Ross held a 100k per ticket fundraiser for Donald Trump's reelection. Related owns Equinox, Soul Cycle etc.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "soulcycle",
+      "brand": "SoulCycle",
+      "description": "Billionaire chairman of Related Companies Stephen M. Ross held a 100k per ticket fundraiser for Donald Trump's reelection. Related owns Equinox, Soul Cycle etc.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "pureyoga",
+      "brand": "PURE yoga",
+      "description": "Billionaire chairman of Related Companies Stephen M. Ross held a 100k per ticket fundraiser for Donald Trump's reelection. Related owns Equinox, Soul Cycle etc.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "blinkfitness",
+      "brand": "Blink Fitness",
+      "description": "Billionaire chairman of Related Companies Stephen M. Ross held a 100k per ticket fundraiser for Donald Trump's reelection. Related owns Equinox, Soul Cycle etc.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "yorkvilleclub",
+      "brand": "The Yorkville Club",
+      "description": "Billionaire chairman of Related Companies Stephen M. Ross held a 100k per ticket fundraiser for Donald Trump's reelection. Related owns Equinox, Soul Cycle etc.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "ghpalmer",
+      "brand": "G.H. Palmer & Associates",
+      "description": "Geoffrey Palmer, one of Los Angeles' largest landlords, is one of Donald Trump’s largest political donors. In July 2016 he donated $2 million to pro-Trump Super PAC Rebuilding America Now.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "hobbylobby",
+      "brand": "Hobby Lobby",
+      "description": "In September 2016 Oklahoma-based billionaire and Hobby Lobby CEO David Green urged Americans to support Donald Trump.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "hsn",
+      "brand": "HSN",
+      "description": "Carries official Trump commemorative coins and similar",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "abcSupply",
+      "brand": "ABC Supply",
+      "description": "Co-founder was vice chair of Trump Victory committee",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "att",
+      "brand": "AT&T",
+      "description": "Donated over $2 million in cash and services to Trump's inauguration",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "bankOfAmerica",
+      "brand": "Bank of America ",
+      "description": "Donated $1 million to Trump's inauguration",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "bpCorporation",
+      "brand": "BP Corporation",
+      "description": "Donated $500k to Trump's inauguration",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "charlesSchwab",
+      "brand": "Charles Schwab",
+      "description": "Chairman Charles Schwab donated $1 million to Trump's inauguration",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "esteeLauder",
+      "brand": "Estee Lauder",
+      "description": "Ron Lauder donated $100,000 to Trump Victory committee",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "lasVegas",
+      "brand": "Las Vegas Sands",
+      "description": "Founder made a $25 million donation to Trump's presidential campaign",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "lendingTree",
+      "brand": "LendingTree",
+      "description": "CEO raised funds for Trump campaign or Trump PAC",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "llBean",
+      "brand": "LL Bean",
+      "description": "Board Member Linda Bean raised funds for Trump",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "marvel",
+      "brand": "Marvel Entertainment",
+      "description": "Chair Isaac Perlmutter donated $360,600 to Trump Victory committee",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "merrillLynch",
+      "brand": "Merrill Lynch",
+      "description": "Donated $1 million to Trump's inauguration",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "millerCoors",
+      "brand": "MillerCoors",
+      "description": "Board Member Peter Coors raised funds for Trump or Trump PAC",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "nascar",
+      "brand": "NASCAR",
+      "description": "Frm CEO Brian France endored Trump during 2016 election cycle",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "nathans",
+      "brand": "Nathan's Famous",
+      "description": "Chairman raised funds for both Trump's election and reelection campaigns",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "newBalance",
+      "brand": "New Balance",
+      "description": "Chairman donated $400,000 to Trump's campaign",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "shellOil",
+      "brand": "Shell Oil",
+      "description": "Shell paid employees to attend Trump rally in August 2019",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "ufc",
+      "brand": "UFC",
+      "description": "President endorsed Trump in 2016 election cycle",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "uline",
+      "brand": "Uline",
+      "description": "President raised funds for Trump presidential campaign",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "venetian",
+      "brand": "Venetian",
+      "description": "Founder made a $25 million donation to Trump's presidential campaign",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "wegmans",
+      "brand": "Wegmans",
+      "description": "Carries Trump Wine",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "wwe",
+      "brand": "WWE",
+      "description": "Co-Founder donated $7 million to Trump campaign",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "louisVuitton",
+      "brand": "Louis Vuitton",
+      "description": "French businessman and chairman of LVMH, Bernard Arnault, did a photo op with the Trump administration at a Louis Vuitton workshop in Texas.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "dior",
+      "brand": "Dior",
+      "description": "French businessman and chairman of LVMH, Bernard Arnault, did a photo op with the Trump administration at a Louis Vuitton workshop in Texas.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "givenchy",
+      "brand": "Givenchy",
+      "description": "French businessman and chairman of LVMH, Bernard Arnault, did a photo op with the Trump administration at a Louis Vuitton workshop in Texas.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "marcJacobs",
+      "brand": "Marc Jacobs",
+      "description": "French businessman and chairman of LVMH, Bernard Arnault, did a photo op with the Trump administration at a Louis Vuitton workshop in Texas.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    },
+    {
+      "id": "fenty",
+      "brand": "Fenty",
+      "description": "French businessman and chairman of LVMH, Bernard Arnault, did a photo op with the Trump administration at a Louis Vuitton workshop in Texas.",
+      "sourceDescription": "#GrabYourWallet",
+      "source": "https://grabyourwallet.org/Boycott%20These%20Companies.html"
+    }
+  ]
